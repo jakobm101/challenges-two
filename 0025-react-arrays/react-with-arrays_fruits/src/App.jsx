7fb45ad29e49a699bd5152a29c4c4d3595f695b6
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-20 12:15:53 +0200

    task 1 done: created array of objects

--- 0025-react-arrays/react-with-arrays_fruits/src/App.jsx
+++ 0025-react-arrays/react-with-arrays_fruits/src/App.jsx
@@ -2,6 +2,16 @@ import Card from "./components/Card";
 
 export default function App() {
   const fruits = [];
+  const names = ["banana", "apple", "cherry", "chili", "tomato"];
+  const colors = ["yellow", "red", "red", "red", "Tomato"];
+  names.map(()=> fruits.push({}))
+  fruits.map((fruit, index) => {
+    fruit["name"] = names[index];
+    fruit['colors'] = colors[index];
+    fruit['index'] = index
+  });
+  console.log({fruits});
+  
 
   return (
     <div className="app">
