a0712e97bd0ceaca5719812dea89414c7ac38419
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-23 11:55:15 +0200

    Done: delete

--- 0027-react-state-3/react-state-3_tags/src/App.jsx
+++ 0027-react-state-3/react-state-3_tags/src/App.jsx
@@ -5,11 +5,16 @@ import List from "./components/List";
 
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+  const handleDelete = (tag) => {
+    const newTags = tags.filter((currentTag) => currentTag !== tag)
+    setTags(newTags)
+  }
+  
 
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={""} />
+      <List tags={tags} onDeleteTag={handleDelete} />
     </main>
   );
 }
