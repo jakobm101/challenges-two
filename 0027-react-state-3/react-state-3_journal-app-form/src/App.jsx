e59b2e5a70e0087cc1232d0fda0ef633def8c3a7
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-23 12:09:00 +0200

    importing useState and uid

--- 0027-react-state-3/react-state-3_journal-app-form/src/App.jsx
+++ 0027-react-state-3/react-state-3_journal-app-form/src/App.jsx
@@ -3,8 +3,14 @@ import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { uid } from "uid";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+  const fakeId = uid();
+  console.log(data,setData, fakeId);
+  
   return (
     <div className="app">
       <Header />
