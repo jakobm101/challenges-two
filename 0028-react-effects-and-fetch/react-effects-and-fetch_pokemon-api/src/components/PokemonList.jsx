0f2f0cf256331321208c2b0225b644db65f663db
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-23 15:52:07 +0200

    refactor

--- 0028-react-effects-and-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
+++ 0028-react-effects-and-fetch/react-effects-and-fetch_pokemon-api/src/components/PokemonList.jsx
@@ -3,17 +3,14 @@ import { useState, useEffect } from "react";
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
 
-  useEffect(() => {
-    async function loadPokemon() {
-      try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-        const data = await response.json();
-        setPokemon(data.results);
-      } catch (error) {
-        console.log(error);
-      }
+  useEffect(() => async () => {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+      const data = await response.json();
+      setPokemon(data.results);
+    } catch (error) {
+      console.log(error);
     }
-    loadPokemon()
   }, []);
 
   return (
