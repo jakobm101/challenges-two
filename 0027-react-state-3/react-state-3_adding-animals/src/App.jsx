711d3a3cb61f2511ab72ac65ca6df1043594249a
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-23 11:38:30 +0200

    importing uid

--- 0027-react-state-3/react-state-3_adding-animals/src/App.jsx
+++ 0027-react-state-3/react-state-3_adding-animals/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {uid} from "uid";
 import "./styles.css";
 import Form from "./components/Form";
 import List from "./components/List";
@@ -26,6 +27,7 @@ export default function App() {
 
   function handleAddAnimal(newAnimal) {
     console.log(newAnimal);
+    
   }
 
   return (
