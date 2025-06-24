f68db5813bd90bba9b2e5d7bae8ffaec677343e8
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-06-24 11:37:04 +0200

    removed superfluos variable

--- 0029-react-with-local-storage/react-with-local-storage_dice/src/components/D6Button/index.jsx
+++ 0029-react-with-local-storage/react-with-local-storage_dice/src/components/D6Button/index.jsx
@@ -2,11 +2,9 @@ import D6 from "../D6";
 import "./D6Button.css";
 
 export function D6Button({value, onRoll}) {
-  const currentRollValue = value;
-
   return (
     <button className="d6-button" type="button" onClick={onRoll}>
-      <D6 value={currentRollValue} />
+      <D6 value={value} />
     </button>
   );
 }
