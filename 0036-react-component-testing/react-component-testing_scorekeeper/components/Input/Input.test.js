b40cc12dc18e3c444a8bac54b604729aa44fea81
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-02 10:58:28 +0200

    pnpm und dl

--- 0036-react-component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ 0036-react-component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,21 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  render(<Input placeholder={"some text"} labelText={"labelText"} />);
+  const inputElement = screen.getByRole("textbox");
+  expect(inputElement).toHaveAttribute("placeholder", "some text");
+  const label = screen.getByText("labelText");
+  expect(label).toBeInTheDocument();
+});
 
-test("calls callback on every user input", async () => {});
+test("calls callback on every user input", async () => {
+  const handleChange = jest.fn();
+  render(<Input onChange={handleChange} />);
+  // await user.click() // click is probably wrong
+  const inputField = screen.getByRole("textbox");
+  const user = userEvent.setup();
+  await user.type(inputField, "hi");
+
+  expect(handleChange).toHaveBeenCalledTimes(2);
+});
