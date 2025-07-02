d66fbc939893e39d3d64c5e964168e21e3450bfc
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-02 12:20:59 +0200

    global state for light

--- 0037-react-global-state/react-global-state_lights/pages/_app.js
+++ 0037-react-global-state/react-global-state_lights/pages/_app.js
@@ -1,7 +1,28 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
+
+const rooms = [
+  "Living Room",
+  "Kitchen",
+  "Bedroom",
+  "Bathroom",
+  "Garage",
+  "Porch",
+  "Garden",
+  "Office",
+];
+
+const initialLights = rooms.map((room, index) => {
+  return { name: room, id: index, isOn: false };
+});
 
 export default function App({ Component, pageProps }) {
+  const [lights, setLights] = useState(initialLights);
+  const countLightsOn = lights.reduce((acc, curr) => {
+    return acc + curr.isOn;
+  }, 0);
+
   return (
     <Layout>
       <GlobalStyle />
