9151ea14a0a0530e8f9408b49f892a17ee6e04d3
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-05 10:45:32 +0200

    SWR logs correct data

--- 0038-react-fetching-SWR/react-data-fetching_star-wars/pages/characters/1.js
+++ 0038-react-fetching-SWR/react-data-fetching_star-wars/pages/characters/1.js
@@ -1,8 +1,14 @@
+import useSWR from "swr";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
 
 export default function Character() {
   const id = 1;
+  const url = 'https://swapi.py4e.com/api/people/1'
+  const fetcher = (url) => fetch(url).then(response => response.json())
+  const { data } = useSWR(url, fetcher)
+  console.log(data);
+  
 
   return (
     <Layout>
