7098c68a475facbdd0fba1cb9a9effbd75acda86
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-09 14:54:13 +0200

    stuff

--- 0039-backend-node-http/backend-basics_chance/server.js
+++ 0039-backend-node-http/backend-basics_chance/server.js
@@ -1,20 +1,20 @@
-import { createServer } from 'node:http'
-import Chance from 'chance'
+import { createServer } from "node:http";
+import Chance from "chance";
 
-const chance = new Chance
-const line = () => `Hello, my name is ${chance.name()} and I am ${chance.age()} years old. I am a ${chance.profession()}.`
+const chance = new Chance();
+const line = () =>
+  `Hello, my name is ${chance.name()} and I am ${chance.age()} years old. I am a ${chance.profession()}.`;
 
+const server = createServer((request, response) => {
+  switch (request.url) {
+    case "/":
+      response.statusCode = 200;
+      response.end(line());
+      break;
+    default:
+      response.statusCode = 404;
+      response.end("404 haha upsi");
+  }
+});
 
-const server = createServer((request, response)=> {
- switch(request.url){
-         case '/':
-         response.statusCode = 200
-         response.end(line())
-            break;
-    default: 
-    response.statusCode = 404
-    response.end('404 haha upsi')
- } 
-})
-
-export default server
+export default server;
