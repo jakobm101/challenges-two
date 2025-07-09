8269f46f696cbb161c43f961daa362b9d31b87d8
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-09 10:38:44 +0200

    Server listens and responds

--- 0039-backend-node-http/backend-basics_greeting/server.js
+++ 0039-backend-node-http/backend-basics_greeting/server.js
@@ -1,3 +1,8 @@
 import { createServer } from "node:http";
 
 // export const server = …
+const server = createServer((request, response) => {
+    response.end(`Hello, here is Jimmy`)
+})
+
+export default server
