6f75c5352169837989251cca52118f20e28ea78a
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-09 16:32:22 +0200

    front end test success

--- 0040-backend-api-routes/backend-api-routes_random-character/pages/api/index.js
+++ 0040-backend-api-routes/backend-api-routes_random-character/pages/api/index.js
@@ -1,3 +1,9 @@
+import Chance from "chance";
+
+const chance = new Chance
+console.log('⭐️',chance.name());
+
+
 export default function handler(req, res) {
-  res.status(200).json({ body: "hi" });
+  res.status(200).json({name: chance.name(), birthday: chance.birthday(), 'soul animal': chance.animal()});
 }
--- 0040-backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ 0040-backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,9 @@
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  return (<>
+    <h1>Character Creator</h1>
+    <h4>Fantastic People from Imagi Nation</h4>
+    <h2>Name name</h2>
+    <p>birth</p>
+    <p>animal</p>
+  </>)
 }
