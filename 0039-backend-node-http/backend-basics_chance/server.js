3aaddb1bf6130cdc03a0959e728e2cc83f7c8ba3
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-09 11:25:56 +0200

    chance works

--- 0039-backend-node-http/backend-basics_chance/server.js
+++ 0039-backend-node-http/backend-basics_chance/server.js
@@ -1,7 +1,17 @@
 import { createServer } from 'node:http'
+import Chance from 'chance'
+
+const chance = new Chance
+const line = `Hello, my name is ${chance.name()} and I am ${chance.age()} years old. I am a ${chance.profession()}.`
+// console.log('testing chance ', line);
+
 
 const server = createServer((request, response)=> {
  switch(request.url){
+         case '/':
+         response.statusCode = 200
+         response.end('home')
+            break;
     default: 
     response.statusCode = 404
     response.end('404 haha upsi')
