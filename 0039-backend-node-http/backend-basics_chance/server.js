8556e62979a534f50f8ee7aa6648a804054be563
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-09 11:36:41 +0200

    random from anon func

--- 0039-backend-node-http/backend-basics_chance/server.js
+++ 0039-backend-node-http/backend-basics_chance/server.js
@@ -2,14 +2,14 @@ import { createServer } from 'node:http'
 import Chance from 'chance'
 
 const chance = new Chance
-let line = `Hello, my name is ${chance.name()} and I am ${chance.age()} years old. I am a ${chance.profession()}.`
+const line = () => `Hello, my name is ${chance.name()} and I am ${chance.age()} years old. I am a ${chance.profession()}.`
 
 
 const server = createServer((request, response)=> {
  switch(request.url){
          case '/':
          response.statusCode = 200
-         response.end(`Hello, my name is ${chance.name()} and I am ${chance.age()} years old. I am a ${chance.profession()}.`)
+         response.end(line())
             break;
     default: 
     response.statusCode = 404
