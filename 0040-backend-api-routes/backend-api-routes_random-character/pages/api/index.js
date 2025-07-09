c51b26c4030bf7aadd6e3ef0e1dacc1d306f1d10
Jakob-M-101 <20402329+jakobm101@users.noreply.github.com>
2025-07-09 18:27:03 +0200

    backend api routes random character (#33)
    * front end test success
    
    * done

--- 0040-backend-api-routes/backend-api-routes_random-character/pages/api/index.js
+++ 0040-backend-api-routes/backend-api-routes_random-character/pages/api/index.js
@@ -1,3 +1,13 @@
+import Chance from "chance";
+
+const chance = new Chance();
+
 export default function handler(req, res) {
-  res.status(200).json({ body: "hi" });
+  res.status(200).json({
+    name: chance.name(),
+    birthday: chance.birthday(),
+    "soul animal": chance.animal(),
+    email: chance.email(),
+    city: chance.city(),
+  });
 }
--- 0040-backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ 0040-backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,18 @@
+import useSWR from "swr";
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data: person, isLoading } = useSWR(`/api/`, (url) =>
+    fetch(url).then((res) => res.json())
+  );
+  if(isLoading) return 'is Loading'
+  return (
+    <>
+      <h1>Fantastic People from Imagi Nation</h1>
+      <h2>{person.name}</h2>
+      <h4>Soul Animal: {person['soul animal']}</h4>
+      <p>Birthday: {person.birthday}</p>
+    <p>City: {person.city}</p>
+    <p>Email: {person.email}</p>
+    </>
+  );
 }
