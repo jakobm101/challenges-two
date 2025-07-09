8c22f702c16748c1592d8a8552104997d31e1fab
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-09 11:02:59 +0200

    four urls responding

--- 0039-backend-node-http/backend-basics_routes/server.js
+++ 0039-backend-node-http/backend-basics_routes/server.js
@@ -1,3 +1,23 @@
 import { createServer } from "node:http";
 
-// export const server = …
+const server = createServer((request, response) => {
+  switch (request.url) {
+    case "/":
+      response.statusCode = 200;
+      response.end("Welcome home");
+      break;
+    case "/fish":
+      response.statusCode = 200;
+      response.end("fish");
+      break;
+    case "/fish2":
+      response.statusCode = 200;
+      response.end("fish2");
+      break;
+    default:
+      response.statusCode = 404;
+      response.end("404 oh no. what is going on?");
+  }
+});
+
+export default server;
