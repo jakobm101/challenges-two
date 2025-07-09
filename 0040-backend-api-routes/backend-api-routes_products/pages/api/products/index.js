1e20150885ebbfb040e851b420e5048c368a1ba7
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-09 15:13:01 +0200

    all products sent via json

--- 0040-backend-api-routes/backend-api-routes_products/pages/api/products/index.js
+++ 0040-backend-api-routes/backend-api-routes_products/pages/api/products/index.js
@@ -1,7 +1,8 @@
-import { getAllProducts } from "@/services/productServices";
+import { getAllProducts } from "@/services/productServices.js";
 
-export default function handler(req, res){
-    console.log('⭐️ api online');
-    
-    res.status('200').json({body:"hi"})
+export default function handler(req, res) {
+  console.log("⭐️ api online");
+  console.log(getAllProducts());
+
+  res.status("200").json(getAllProducts());
 }
