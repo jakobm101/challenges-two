602aa5b077c661645aa50b59e837ae1efc1e262f
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-09 15:39:14 +0200

    data gets fetched

--- 0040-backend-api-routes/backend-api-routes_products/pages/products/index.js
+++ 0040-backend-api-routes/backend-api-routes_products/pages/products/index.js
@@ -1,7 +1,14 @@
+import useSWR from "swr";
+
 export default function Products() {
+  const { data } = useSWR("/api/products/", (url) =>
+    fetch(url).then((r) => r.json())
+  );
+    console.log('data', data);
+    
   return (
     <>
-      <h1>All the Products  </h1>
+      <h1>All the Products </h1>
     </>
   );
 }
